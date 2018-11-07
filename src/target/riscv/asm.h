6940507b5b966b17bfdb46011a30749e761981ea
Pavel S. Smirnov <pavel.smirnov@syntacore.com>
2018-11-07 16:28:01 +0300

    chore(src/target/riscv/asm.h): using __attribute__  w/o prototype, proper Doxygen markup instead dummy stars.

--- src/target/riscv/asm.h
+++ src/target/riscv/asm.h
@@ -3,36 +3,44 @@
 
 #include "riscv.h"
 
-/*** Version-independent functions that we don't want in the main address space. ***/
+#include <assert.h>
 
-static uint32_t load(const struct target *target, unsigned int rd,
-		unsigned int base, uint16_t offset) __attribute__ ((unused));
-static uint32_t load(const struct target *target, unsigned int rd,
-		unsigned int base, uint16_t offset)
+/** Version-independent functions that we don't want in the main address space.
+	@{
+*/
+static uint32_t __attribute__((unused))
+load(const struct target *target, unsigned rd, unsigned base, uint16_t offset)
 {
 	switch (riscv_xlen(target)) {
-		case 32:
-			return lw(rd, base, offset);
-		case 64:
-			return ld(rd, base, offset);
+	case 32:
+		return lw(rd, base, offset);
+
+	case 64:
+		return ld(rd, base, offset);
+
+		/** @bug no default case*/
 	}
+
 	assert(0);
-	return 0; /* Silence -Werror=return-type */
+	return 0; /*< Silence -Werror=return-type */
 }
 
-static uint32_t store(const struct target *target, unsigned int src,
-		unsigned int base, uint16_t offset) __attribute__ ((unused));
-static uint32_t store(const struct target *target, unsigned int src,
-		unsigned int base, uint16_t offset)
+static uint32_t __attribute__((unused))
+store(const struct target *target, unsigned src, unsigned base, uint16_t offset)
 {
 	switch (riscv_xlen(target)) {
-		case 32:
-			return sw(src, base, offset);
-		case 64:
-			return sd(src, base, offset);
+	case 32:
+		return sw(src, base, offset);
+
+	case 64:
+		return sd(src, base, offset);
+
+		/** @bug no default case*/
 	}
+
 	assert(0);
-	return 0; /* Silence -Werror=return-type */
+	return 0; /*< Silence -Werror=return-type */
 }
+/** @} */
 
 #endif
