2966012b5d64efe7bda880e92934c84e39c1c847
Pavel S. Smirnov <pavel.smirnov@syntacore.com>
2018-11-14 19:31:35 +0300

    chore(src/target/riscv/opcodes.h) add assert and __attribute__((const))

--- src/target/riscv/opcodes.h
+++ src/target/riscv/opcodes.h
@@ -9,261 +9,418 @@
 #define S0      8
 #define S1      9
 
-static inline uint32_t bits(uint32_t value, unsigned hi, unsigned lo)
+static inline uint32_t
+__attribute__((const))
+bits(uint32_t const value,
+	unsigned const hi,
+	unsigned const lo)
 {
 	return (value >> lo) & ((1 << (hi + 1 - lo)) - 1);
 }
 
-static inline uint32_t bit(uint32_t value, unsigned b)
+static inline uint32_t
+__attribute__((const))
+bit(uint32_t const value,
+	const unsigned b)
 {
-	return (value >> b) & 1;
+	return 1 & (value >> b);
 }
 
-static inline uint32_t jal(unsigned rd, uint32_t imm)
+static inline uint32_t
+__attribute__((const))
+jal(unsigned const rd,
+	uint32_t const imm)
 {
+	assert(rd < 32);
 	return
-		(bit(imm, 20) << 31) |
-		(bits(imm, 10, 1) << 21) |
-		(bit(imm, 11) << 20) |
-		(bits(imm, 19, 12) << 12) |
-		(rd << 7) |
+		bit(imm, 20) << 31 |
+		bits(imm, 10, 1) << 21 |
+		bit(imm, 11) << 20 |
+		bits(imm, 19, 12) << 12 |
+		rd << 7 |
 		MATCH_JAL;
 }
 
-static inline uint32_t csrsi(unsigned csr, uint16_t imm)
+static inline uint32_t
+__attribute__((const))
+csrsi(unsigned const csr,
+	uint16_t const imm)
 {
 	return
-		(csr << 20) |
-		(bits(imm, 4, 0) << 15) |
+		csr << 20 |
+		bits(imm, 4, 0) << 15 |
 		MATCH_CSRRSI;
 }
 
-static inline uint32_t sw(unsigned src, unsigned base, uint16_t offset)
+static inline uint32_t
+__attribute__((const))
+sw(unsigned const src,
+	unsigned const base,
+	uint16_t const offset)
 {
+	assert(src < 32 && base < 32);
 	return
-		(bits(offset, 11, 5) << 25) |
-		(src << 20) |
-		(base << 15) |
-		(bits(offset, 4, 0) << 7) |
+		bits(offset, 11, 5) << 25 |
+		src << 20 |
+		base << 15 |
+		bits(offset, 4, 0) << 7 |
 		MATCH_SW;
 }
 
-static inline uint32_t sd(unsigned src, unsigned base, uint16_t offset)
+static inline uint32_t
+__attribute__((const))
+sd(unsigned const src,
+	unsigned const base,
+	uint16_t const offset)
 {
+	assert(src < 32 && base < 32);
 	return
-		(bits(offset, 11, 5) << 25) |
-		(src << 20) |
-		(base << 15) |
-		(bits(offset, 4, 0) << 7) |
+		bits(offset, 11, 5) << 25 |
+		src << 20 |
+		base << 15 |
+		bits(offset, 4, 0) << 7 |
 		MATCH_SD;
 }
 
-static inline uint32_t sh(unsigned src, unsigned base, uint16_t offset)
+static inline uint32_t
+__attribute__((const))
+sh(unsigned const src,
+	unsigned const base,
+	uint16_t const offset)
 {
+	assert(src < 32 && base < 32);
 	return
-		(bits(offset, 11, 5) << 25) |
-		(src << 20) |
-		(base << 15) |
-		(bits(offset, 4, 0) << 7) |
+		bits(offset, 11, 5) << 25 |
+		src << 20 |
+		base << 15 |
+		bits(offset, 4, 0) << 7 |
 		MATCH_SH;
 }
 
-static inline uint32_t sb(unsigned src, unsigned base, uint16_t offset)
+static inline uint32_t
+__attribute__((const))
+sb(unsigned const src,
+	unsigned const base,
+	uint16_t const offset)
 {
+	assert(src < 32 && base < 32);
 	return
-		(bits(offset, 11, 5) << 25) |
-		(src << 20) |
-		(base << 15) |
-		(bits(offset, 4, 0) << 7) |
+		bits(offset, 11, 5) << 25 |
+		src << 20 |
+		base << 15 |
+		bits(offset, 4, 0) << 7 |
 		MATCH_SB;
 }
 
-static inline uint32_t ld(unsigned rd, unsigned base, uint16_t offset)
+/** @bug @c offset should be signed */
+static inline uint32_t
+__attribute__((const))
+ld(unsigned const rd,
+	unsigned const base,
+	uint16_t const offset)
 {
+	assert(rd < 32 && base < 32);
 	return
-		(bits(offset, 11, 0) << 20) |
-		(base << 15) |
-		(bits(rd, 4, 0) << 7) |
+		bits(offset, 11, 0) << 20 |
+		base << 15 |
+		bits(rd, 4, 0) << 7 |
 		MATCH_LD;
 }
 
-static inline uint32_t lw(unsigned rd, unsigned base, uint16_t offset)
+static inline uint32_t
+__attribute__((const))
+lw(unsigned const rd,
+	unsigned const base,
+	uint16_t const offset)
 {
+	assert(rd < 32 && base < 32);
 	return
-		(bits(offset, 11, 0) << 20) |
-		(base << 15) |
-		(bits(rd, 4, 0) << 7) |
+		bits(offset, 11, 0) << 20 |
+		base << 15 |
+		bits(rd, 4, 0) << 7 |
 		MATCH_LW;
 }
 
-static inline uint32_t lh(unsigned rd, unsigned base, uint16_t offset)
+static inline uint32_t
+__attribute__((const))
+lh(unsigned const rd,
+	unsigned const base,
+	uint16_t const offset)
 {
+	assert(rd < 32 && base < 32);
 	return
-		(bits(offset, 11, 0) << 20) |
-		(base << 15) |
-		(bits(rd, 4, 0) << 7) |
+		bits(offset, 11, 0) << 20 |
+		base << 15 |
+		bits(rd, 4, 0) << 7 |
 		MATCH_LH;
 }
 
-static inline uint32_t lb(unsigned rd, unsigned base, uint16_t offset)
+static inline uint32_t
+__attribute__((const))
+lb(unsigned const rd,
+	unsigned const base,
+	uint16_t const offset)
 {
+	assert(base < 32 && rd < 32);
 	return
-		(bits(offset, 11, 0) << 20) |
-		(base << 15) |
-		(bits(rd, 4, 0) << 7) |
+		bits(offset, 11, 0) << 20 |
+		base << 15 |
+		bits(rd, 4, 0) << 7 |
 		MATCH_LB;
 }
 
-static inline uint32_t csrw(unsigned source, unsigned csr)
+static inline uint32_t
+__attribute__((const))
+csrw(unsigned const source,
+	unsigned const csr)
 {
+	assert(source < 32);
 	return
-		(csr << 20) |
-		(source << 15) |
+		csr << 20 |
+		source << 15 |
 		MATCH_CSRRW;
 }
 
-static inline uint32_t addi(unsigned dest, unsigned src, uint16_t imm)
+static inline uint32_t
+__attribute__((const))
+addi(unsigned const dest,
+	unsigned const src,
+	uint16_t const imm)
 {
+	assert(src < 32 && dest < 32);
 	return
-		(bits(imm, 11, 0) << 20) |
-		(src << 15) |
-		(dest << 7) |
+		bits(imm, 11, 0) << 20 |
+		src << 15 |
+		dest << 7 |
 		MATCH_ADDI;
 }
 
-static inline uint32_t csrr(unsigned rd, unsigned csr)
+/** @bug @c csr is 12 bits only */
+static inline uint32_t
+__attribute__((const))
+csrr(unsigned const rd,
+	unsigned const csr)
 {
-	return (csr << 20) | (rd << 7) | MATCH_CSRRS;
-}
-
-static inline uint32_t csrrs(unsigned rd, unsigned rs, unsigned csr)
-{
-	return (csr << 20) | (rs << 15) | (rd << 7) | MATCH_CSRRS;
-}
-
-static inline uint32_t csrrw(unsigned rd, unsigned rs, unsigned csr)
-{
-	return (csr << 20) | (rs << 15) | (rd << 7) | MATCH_CSRRW;
-}
-
-static inline uint32_t fsw(unsigned src, unsigned base, uint16_t offset)
-{
+	assert(rd < 32);
 	return
-		(bits(offset, 11, 5) << 25) |
-		(bits(src, 4, 0) << 20) |
-		(base << 15) |
-		(bits(offset, 4, 0) << 7) |
+		csr << 20 |
+		rd << 7 |
+		MATCH_CSRRS;
+}
+
+static inline uint32_t
+__attribute__((const))
+csrrs(unsigned const rd,
+	unsigned const rs,
+	unsigned const csr)
+{
+	assert(rs < 32 && rd < 32);
+	return
+		csr << 20 |
+		rs << 15 |
+		rd << 7 |
+		MATCH_CSRRS;
+}
+
+static inline uint32_t
+__attribute__((const))
+csrrw(unsigned const rd,
+	unsigned const rs,
+	unsigned const csr)
+{
+	assert(rs < 32 && rd < 32);
+	return
+		csr << 20 |
+		rs << 15 |
+		rd << 7 |
+		MATCH_CSRRW;
+}
+
+static inline uint32_t
+__attribute__((const))
+fsw(unsigned const src,
+	unsigned const base,
+	uint16_t const offset)
+{
+	assert(src < 32 && base < 32);
+	return
+		bits(offset, 11, 5) << 25 |
+		bits(src, 4, 0) << 20 |
+		base << 15 |
+		bits(offset, 4, 0) << 7 |
 		MATCH_FSW;
 }
 
-static inline uint32_t fsd(unsigned src, unsigned base, uint16_t offset)
+static inline uint32_t
+fsd(unsigned const src,
+	unsigned const base,
+	uint16_t const offset)
 {
+	assert(src < 32 && base < 32);
 	return
-		(bits(offset, 11, 5) << 25) |
-		(bits(src, 4, 0) << 20) |
-		(base << 15) |
-		(bits(offset, 4, 0) << 7) |
+		bits(offset, 11, 5) << 25 |
+		bits(src, 4, 0) << 20 |
+		base << 15 |
+		bits(offset, 4, 0) << 7 |
 		MATCH_FSD;
 }
 
-static inline uint32_t flw(unsigned dest, unsigned base, uint16_t offset)
+static inline uint32_t
+flw(unsigned const dest,
+	unsigned const base,
+	uint16_t const offset)
 {
+	assert(dest < 32 && base < 32);
 	return
-		(bits(offset, 11, 0) << 20) |
-		(base << 15) |
-		(bits(dest, 4, 0) << 7) |
+		bits(offset, 11, 0) << 20 |
+		base << 15 |
+		bits(dest, 4, 0) << 7 |
 		MATCH_FLW;
 }
 
-static inline uint32_t fld(unsigned dest, unsigned base, uint16_t offset)
+static inline uint32_t
+__attribute__((const))
+fld(unsigned const dest,
+	unsigned const base,
+	uint16_t const offset)
 {
-	return (bits(offset, 11, 0) << 20) |
-		(base << 15) |
-		(bits(dest, 4, 0) << 7) |
+	assert(dest < 32 && base < 32);
+	return
+		bits(offset, 11, 0) << 20 |
+		base << 15 |
+		bits(dest, 4, 0) << 7 |
 		MATCH_FLD;
 }
 
-static inline uint32_t fmv_x_w(unsigned dest, unsigned src)
+static inline uint32_t
+__attribute__((const))
+fmv_x_w(unsigned const dest,
+	unsigned const src)
 {
-	return src << 15 |
+	assert(src < 32 && dest < 32);
+	return
+		src << 15 |
 		dest << 7 |
 		MATCH_FMV_X_W;
 }
 
-static inline uint32_t fmv_x_d(unsigned dest, unsigned src)
+static inline uint32_t
+__attribute__((const))
+fmv_x_d(unsigned const dest,
+	unsigned const src)
 {
-	return src << 15 |
+	assert(src < 32 && dest < 32);
+	return
+		src << 15 |
 		dest << 7 |
 		MATCH_FMV_X_D;
 }
 
-static inline uint32_t fmv_w_x(unsigned dest, unsigned src)
+static inline uint32_t
+__attribute__((const))
+fmv_w_x(unsigned const dest,
+	unsigned const src)
 {
-	return src << 15 |
+	assert(src < 32 && dest < 32);
+	return
+		src << 15 |
 		dest << 7 |
 		MATCH_FMV_W_X;
 }
 
-static inline uint32_t fmv_d_x(unsigned dest, unsigned src)
+static inline uint32_t
+__attribute__((const))
+fmv_d_x(unsigned const dest,
+	unsigned const src)
 {
+	assert(src < 32 && dest < 32);
 	return
 		src << 15 |
 		dest << 7 |
 		MATCH_FMV_D_X;
 }
 
-static inline uint32_t ebreak(void)
+static inline uint32_t
+__attribute__((const))
+ebreak(void)
 {
 	return MATCH_EBREAK;
 }
-static inline uint32_t ebreak_c(void)
+
+/** @bug @c ebreak_c is 16 bit only*/
+static inline uint32_t
+__attribute__((const))
+ebreak_c(void)
 {
 	return MATCH_C_EBREAK;
 }
 
-static inline uint32_t wfi(void)
+static inline uint32_t
+__attribute__((const))
+wfi(void)
 {
 	return MATCH_WFI;
 }
 
-static inline uint32_t fence_i(void)
+static inline uint32_t
+__attribute__((const))
+fence_i(void)
 {
 	return MATCH_FENCE_I;
 }
 
-static inline uint32_t lui(unsigned dest, uint32_t imm)
+static inline uint32_t
+__attribute__((const))
+lui(unsigned const dest,
+	uint32_t const imm)
 {
+	assert(dest < 32);
 	return
-		(bits(imm, 19, 0) << 12) |
-		(dest << 7) |
+		bits(imm, 19, 0) << 12 |
+		dest << 7 |
 		MATCH_LUI;
 }
 
-static inline uint32_t xori(unsigned dest, unsigned src, uint16_t imm)
+static inline uint32_t
+__attribute__((const))
+xori(unsigned const dest,
+	unsigned const src,
+	uint16_t const imm)
 {
+	assert(src < 32 && dest < 32);
 	return
-		(bits(imm, 11, 0) << 20) |
-		(src << 15) |
-		(dest << 7) |
+		bits(imm, 11, 0) << 20 |
+		src << 15 |
+		dest << 7 |
 		MATCH_XORI;
 }
 
-static inline uint32_t srli(unsigned dest, unsigned src, uint8_t shamt)
+static inline uint32_t
+__attribute__((const))
+srli(unsigned const dest,
+	unsigned const src,
+	uint8_t const shamt)
 {
+	assert(src < 32 && dest < 32);
 	return
-		(bits(shamt, 4, 0) << 20) |
-		(src << 15) |
-		(dest << 7) |
+		bits(shamt, 4, 0) << 20 |
+		src << 15 |
+		dest << 7 |
 		MATCH_SRLI;
 }
 
-static inline uint32_t fence(void)
+static inline uint32_t
+__attribute__((const))
+fence(void)
 {
 	return MATCH_FENCE;
 }
 
-static inline uint32_t auipc(unsigned dest)
+static inline uint32_t
+__attribute__((const))
+auipc(unsigned dest)
 {
+	assert(dest < 32);
 	return MATCH_AUIPC | (dest << 7);
 }
 
